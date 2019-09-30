ad322cdc10167654d624e1deec14e90c11043a72
ken5b <52589647+ken5b@users.noreply.github.com>
2019-09-30 12:29:06 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   controllers: { registrations: 'registrations' }
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
-  get '/posts/new', to: 'posts#new'
-  post '/posts', to: 'posts#create'
-  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
     resources :likes, only: %i(create destroy)
